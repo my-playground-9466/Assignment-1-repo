1ebb808c038bb35f20056a35cc66e151c981f026
Kumudi Iroshini <abeygunawardena123@gmail.com>
2023-07-30 02:00:03 +0530

    Complete Task 6-A

---
+++ Demo6_task_a.java
@@ -0,0 +1,12 @@
+public class Demo6_task_a {
+    public static void main(String[] args) {
+
+        String RED = "\033[41m";
+        String GREEn = "\033[42m";
+
+        String RESET = "\033[0m";
+        
+        System.out.printf("%s[%8d%s%s%-8s]%3$s\n",RED,5,RESET,GREEn,"0%");
+    }
+    
+}
