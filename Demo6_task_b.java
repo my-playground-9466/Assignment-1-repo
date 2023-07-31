d2b39bf9e0b8f82a5b0b0f807bb615bc2c53dc1a
Kumudi Iroshini <abeygunawardena123@gmail.com>
2023-07-31 14:38:57 +0530

    Complete Task 6-B

--- Demo6_task_b.java
+++ Demo6_task_b.java
@@ -9,16 +9,16 @@ public class Demo6_task_b {
         int age2 = 30;
 
         String COLOR_BLUE = "\033[34;1m";
-        String COLOR_MAGENTA = "\033[36;1m";
-        String COLOR_ORANGE = "\033[33;0m";
+        String COLOR_PURPLE = "\033[35;1m";
+        String COLOR_ORANGE = "\033[33m";
 
         String RESET = "\033[0;0m";
 
         System.out.printf("%s+%s+%s+\n",COLOR_ORANGE,"-".repeat(10),"-".repeat(5));
-        System.out.printf("|%s%-10s%s|%1$s%5s%3$s|\n",COLOR_BLUE,"NAME",RESET,"AGE");
-        System.out.println("+----------+-----+");
-        System.out.printf("|%-10s|%s%5s%s|\n|%-10s|%2$s%5s%4$s|\n",name1,COLOR_MAGENTA,age1,RESET,name2,age2);
-        System.out.println("+----------+-----+");
+        System.out.printf("|%s%-10s%s|%s%1$s%5s%3$s|\n",COLOR_BLUE,"NAME",COLOR_ORANGE,RESET,"AGE");
+        System.out.printf("%s+%s+%s+\n",COLOR_ORANGE,"-".repeat(10),"-".repeat(5));
+        System.out.printf("|%s%-10s%s|%s%5s%3$s|\n|%1$s%-10s%3$s|%4$s%5s%3$s|\n",RESET,name1,COLOR_ORANGE,COLOR_PURPLE,age1,name2,age2);
+        System.out.printf("%s+%s+%s+\n",COLOR_ORANGE,"-".repeat(10),"-".repeat(5));
 
 
     }
